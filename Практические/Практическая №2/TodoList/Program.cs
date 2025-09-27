5b04b075a8d013ac620a8414c45b06aa39c4eeed
woosubi <waste_01@mail.ru>
2025-09-27 11:20:18 +0300

    Выполнение практической №2

---
+++ Практические/Практическая №2/TodoList/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TodoList
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Введите ваше имя: "); 
+            string firstName = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введите ваш год рождения: ");
+            int birthday = int.Parse(Console.ReadLine());
+
+            string text = "Добавлен пользователь " + firstName + " " + lastName + ", возраст - " + (DateTime.Now.Year - birthday);
+            Console.WriteLine(text);
+        }
+    }
+}
