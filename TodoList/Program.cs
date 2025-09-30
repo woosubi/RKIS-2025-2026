96696b7c1e0fff91a2f1f97f971a6d7b5dfcf2eb
Кирилл Мурза <waste_01@mail.ru>
2025-09-30 19:41:32 +0300

    lesson 3: add todo list functionality

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -17,6 +17,9 @@
             string text = "Добавлен пользователь " + firstName + " " + lastName + ", возраст - " + age;
             Console.WriteLine(text);
 
+            string[] todos = new string[2];
+            int index = 0;
+
             while (true)
             {
                 Console.Write("Введите команду: ");
@@ -40,6 +43,36 @@
                 {
                     Console.WriteLine("Выход из программы.");
                     break;
+                }
+                else if (command.StartsWith("add "))
+                {
+                    string task = command.Split("add ")[1];
+                    if (index == todos.Length)
+                    {
+                        string[] newTodos = new string[todos.Length*2];
+                        for (int i = 0; i < todos.Length; i++)
+                        {
+                            newTodos[i] = todos[i];
+                        }
+
+                        todos = newTodos;
+                    }
+
+                    todos[index] = task;
+                    index++;
+
+                    Console.WriteLine("Добавлена задача: " + task);
+                }
+                else if (command == "view")
+                {
+                    Console.WriteLine("Задачи:");
+                    foreach (string todo in todos)
+                    {
+                        if (!string.IsNullOrEmpty(todo))
+                        {
+                            Console.WriteLine(todo);
+                        }
+                    }
                 }
                 else
                 {
