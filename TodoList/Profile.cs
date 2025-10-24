9bc0ef65ff7822d73d80f140214652099517c0e4
Кирилл Мурза <waste_01@mail.ru>
2025-10-24 19:39:15 +0300

    lesson 6: add profile class

---
+++ TodoList/Profile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList;
+public class Profile
+{
+	public string FirstName { get; private set; }
+	public string LastName { get; private set; }
+	public int BirthYear { get; private set; }
+
+	public Profile(string firstName, string lastName, int birthYear)
+	{
+		FirstName = firstName;
+		LastName = lastName;
+		BirthYear = birthYear;
+	}
+
+	public string GetInfo()
+	{
+		int age = DateTime.Now.Year - BirthYear;
+		return $"{FirstName} {LastName}, возраст {age}";
+	}
+}
