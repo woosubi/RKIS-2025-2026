265edbaa0b182c7fab6d5449876d7d1ec7dc5d04
Кирилл Мурза <waste_01@mail.ru>
2025-10-24 19:19:04 +0300

    lesson 6: add todoitem class

---
+++ TodoList/TodoItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList;
+public class TodoItem
+{
+	public string Text { get; private set; }
+	public bool IsDone { get; private set; }
+	public DateTime LastUpdate { get; private set; }
+
+	public TodoItem(string text)
+	{
+		Text = text;
+		IsDone = false;
+		LastUpdate = DateTime.Now;
+	}
+
+	public void MarkDone()
+	{
+		IsDone = true;
+		LastUpdate = DateTime.Now;
+	}
+
+	public void UpdateText(string newText)
+	{
+		Text = newText;
+		LastUpdate = DateTime.Now;
+	}
+
+	public string GetShortInfo()
+	{
+		string text = Text.Replace("\r", " ").Replace("\n", " ");
+		if (text.Length > 30) text = text[..30] + "...";
+
+		string status = IsDone ? "выполнена" : "не выполнена";
+		return $"{text,-34}|{status,-16}|{LastUpdate:yyyy-MM-dd HH:mm}|";
+	}
+
+	public string GetFullInfo(int index)
+	{
+		string status = IsDone ? "выполнена" : "не выполнена";
+		return $"Полный текст задачи {index}:\n{Text}\nСтатус: {status}\nДата последнего изменения: {LastUpdate:yyyy-MM-dd HH:mm}";
+	}
+}
