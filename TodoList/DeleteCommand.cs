882b023a9d14bdc07f95953b834699df4d373b62
21versa <kerillkucerenko33@gmail.com>
2025-10-31 19:12:43 +0300

    lesson 7: add delete command

---
+++ TodoList/DeleteCommand.cs
@@ -0,0 +1,12 @@
+namespace TodoList;
+
+public class DeleteCommand : ICommand
+{
+	public int TaskIndex { get; set; }
+	public TodoList TodoList { get; set; }
+
+	public void Execute()
+	{
+		TodoList.Delete(TaskIndex);
+	}
+}
