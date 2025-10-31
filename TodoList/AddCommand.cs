699baba4c6a7b1906003e5284449a493421a1aa2
21versa <kerillkucerenko33@gmail.com>
2025-10-31 19:09:57 +0300

    lesson 7: creating add command class

---
+++ TodoList/AddCommand.cs
@@ -0,0 +1,26 @@
+namespace TodoList;
+
+public class AddCommand : ICommand
+{
+	public bool IsMultiline { get; set; }
+	public string TaskText { get; set; }
+	public TodoList TodoList { get; set; }
+
+	public void Execute()
+	{
+		if (IsMultiline)
+		{
+			Console.WriteLine("Введите текст задачи построчно. Завершите ввод командой !end");
+			TaskText = "";
+			while (true)
+			{
+				Console.Write("> ");
+				var line = Console.ReadLine();
+				if (line == "!end") break;
+				TaskText += line + "\n";
+			}
+		}
+
+		TodoList.Add(new TodoItem(TaskText.Trim()));
+	}
+}
