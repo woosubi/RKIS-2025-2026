2ff42b03ca88792a92697b9e4d709694d637cc7a
21versa <kerillkucerenko33@gmail.com>
2025-10-31 19:21:08 +0300

    lesson 7: add profile command

---
+++ TodoList/ProfileCommand.cs
@@ -0,0 +1,11 @@
+namespace TodoList;
+
+public class ProfileCommand : ICommand
+{
+	public Profile Profile { get; set; }
+
+	public void Execute()
+	{
+		Console.WriteLine(Profile.GetInfo());
+	}
+}
