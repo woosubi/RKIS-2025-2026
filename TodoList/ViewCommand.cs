314e9b2b57c5a87912fa6c8f525de2d0aa31d021
21versa <kerillkucerenko33@gmail.com>
2025-10-31 19:28:24 +0300

    lesson 7: add command view

---
+++ TodoList/ViewCommand.cs
@@ -0,0 +1,14 @@
+namespace TodoList;
+
+public class ViewCommand : ICommand
+{
+	public bool ShowIndex { get; set; }
+	public bool ShowStatus { get; set; }
+	public bool ShowDate { get; set; }
+	public TodoList TodoList { get; set; }
+
+	public void Execute()
+	{
+		TodoList.View(ShowIndex, ShowStatus, ShowDate);
+	}
+}
